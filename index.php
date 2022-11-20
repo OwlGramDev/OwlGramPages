debc3ab6028e0fd18e23864c16262ea2c96362f6
Laky64 <35636667+Laky-64@users.noreply.github.com>
2022-11-20 22:34:22 +0100

    First Release

---
+++ index.php
@@ -0,0 +1,178 @@
+<?php
+$SVG_VERSION = 3;
+$CSS_VERSION = 4;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>OwlGram</title>
+    <meta name="viewport" content="width=device-width, user-scalable=0">
+    <link rel="preload" href="//owlgram.org/css/style.css?v=<?php echo $CSS_VERSION; ?>" as="style">
+    <link href="//owlgram.org/css/style.css?v=<?php echo $CSS_VERSION; ?>" rel="stylesheet">
+    <link rel="preload" as="image" href="img/screenshot.webp">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700%7CGoogle+Sans:400,500%7CProduct+Sans:400&amp;display=swap&amp;lang=en" as="style" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700|Google+Sans:400,500|Product+Sans:400&amp;display=swap&amp;lang=en" crossorigin>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="OwlGram" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="right" data-x_margin="18" data-y_margin="18"></script>
+</head>
+<body>
+<div class="header">
+    <div class="header__logo">
+        <div class="h-c-header__company-logo">
+            <div class="h-c-header__company-logo-link" title="OwlGram">
+                <img src="img/owlgram.svg" alt="">
+                <h1 class="top_bar_text">
+                    OwlGram
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="header__aside">
+        <div class="header__aside__buttons">
+            <div class="header__aside__buttons__button">
+                <a class="button button--not-web-only" href="https://files.owlgram.org/OwlGram.apk">
+                    DOWNLOAD
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main">
+    <div class="hero--no_device hero">
+        <div class="hero__content  hero--no_device">
+            <div class="hero__content__main  hero--no_device">
+                <h1 class="hero__content__main__title">
+                    Always Updated,<br>
+                    Customizable,<br>
+                    Fast
+                </h1>
+                <div class="hero__content__main__body">
+                    OwlGram is a third-party client that uses Telegram's API, the world's fastest messaging app.<br>
+                    It's free, powerful, secure and cloud-based.
+                </div>
+                <div class="hero__content__main__buttons">
+                    <div class="hero__content__main__buttons__button">
+                        <a class="button button--not-web-only"  href="https://files.owlgram.org/OwlGram.apk">
+                            DOWNLOAD
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="hero__content__image hero--no_device">
+                <img src="img/screenshot.webp" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="features">
+        <h2 class="features__title">
+            Getting started
+        </h2>
+        <div class="features__container">
+            <div class="feature">
+                <div class="feature__icon">
+                    <img src="img/customizable.svg?version=<?php echo $SVG_VERSION;?>" alt="">
+                </div>
+                <div class="feature__content">
+                    <div class="feature__title">
+                        More Customizable
+                    </div>
+                    <div class="feature__description">
+                        Use System Fonts or Emojis,
+                        make the status bar transparent,
+                        show seconds in time,
+                        play GIFs as Video
+                        and more...
+                    </div>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature__icon">
+                    <img src="img/updated.svg?version=<?php echo $SVG_VERSION;?>" alt="">
+                </div>
+                <div class="feature__content">
+                    <div class="feature__title">
+                        Always updated
+                    </div>
+                    <div class="feature__description">
+                        The OwlGram Team will keep the client always updated to
+                        the latest Telegram Build, moreover there's a Beta Program
+                        which you can test new versions before their official release.
+                    </div>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature__icon">
+                    <img src="img/translator.svg?version=<?php echo $SVG_VERSION;?>" alt="">
+                </div>
+                <div class="feature__content">
+                    <div class="feature__title">
+                        Translator
+                    </div>
+                    <div class="feature__description">
+                        Translate messages and descriptions with 5 translation providers to choose from.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="features__cta" style="display: none !important;">
+            <div class="__buttons__button">
+                <a class="button button--web-only">
+                    Download
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="chapter " id="download">
+        <div class="chapter__content">
+            <div class="chapter__content__icons">
+                <div class="chapter__content__icons__icon">
+                    <img src="img/owlgram.svg" alt="">
+                </div>
+            </div>
+            <div class="chapter__content__title">OwlGram</div>
+            <div class="chapter__content__body">Download OwlGram for free, it's powerful, secure and cloud-based.</div>
+            <div class="chapter__content__buttons">
+                <div class="chapter__content__buttons__button">
+                    <a class="button button--image" href="https://play.google.com/store/apps/details?id=it.owlgram.android">
+                        <img src="img/get_playstore.svg" alt="">
+                    </a>
+                    <a class="button button--image" href="https://appgallery.cloud.huawei.com/marketshare/app/C105849965">
+                        <img src="img/get_appgallery.svg" alt="">
+                    </a>
+                    <a class="button button--image" href="https://install.appcenter.ms/users/owlgram/apps/owlgram/distribution_groups/public">
+                        <img src="img/get_appcenter.svg" alt="">
+                    </a>
+                    <a class="button button--image" href="https://t.me/OwlGramAPKs">
+                        <img src="img/get_telegram.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer__nav">
+            <div class="footer__nav__links">
+                <div class="footer__nav__links__item">
+                    <a rel="”noopener”" href="terms/">Terms & Conditions</a>
+                </div>
+                <div class="footer__nav__links__item">
+                    <a rel="”noopener”" href="https://t.me/OwlGramForum">Telegram Group</a>
+                </div>
+                <div class="footer__nav__links__item">
+                    <a rel="”noopener”" href="https://t.me/OwlGram">Telegram Channel</a>
+                </div>
+                <div class="footer__nav__links__item">
+                    <a rel="”noopener”" href="https://t.me/OwlGramAPKs">APKs Channel</a>
+                </div>
+		 <div class="footer__nav__links__item">
+                    <a rel="”noopener”" href="https://t.me/squirrelnetwork">SquirrelNetwork</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ terms/index.php
@@ -0,0 +1,113 @@
+<html lang="en">
+<head>
+    <title>Terms & Conditions</title>
+    <meta name='viewport' content='width=device-width, user-scalable=0'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700|Google+Sans:400,500|Product+Sans:400&amp;lang=en">
+    <link href="//owlgram.org/css/terms.css?v=1.<?php echo time();?>" rel='stylesheet'>
+</head>
+<body>
+<div class="header">
+    <img src="../img/tos.svg" alt="">
+    <h1>
+        OwlGram Privacy Policy
+    </h1>
+    <p style="color: #202124;font-weight: 500;margin: 0 0 10px;">Effective November 7, 2022</p>
+    <p>
+        This privacy notice for <b>OwlGram</b> ("Company", "we", "us", or "our"), describes how and why we might collect, store, use, and/or share ("process") your information when you use our services ("Services"), such as when you:
+    </p>
+    <ul>
+        <li>
+            <a href="https://owlgram.org/">Download and use our mobile application</a> (OwlGram), or any other application of ours that links to this privacy notice
+        </li>
+        <li>
+            Engage with us in other related ways, including any sales, marketing, or events
+        </li>
+    </ul>
+    <h2>
+        Open Source
+    </h2>
+    <p>
+        OwlGram is Open Source in order to comply with the Telegram API Terms of Use and the GNU GPL License, the code is available at <a href="https://github.com/OwlGramDev/OwlGram">GitHub</a>
+    </p>
+    <h2>
+        Usage of Personal Data
+    </h2>
+    <p>
+        To improve security and for a better experience while using OwlGram, we use Service that may require you to provide us certain information.<br>
+        Below are the Service that we use as third party solutions
+    </p>
+    <ul>
+        <li>
+            <a href="https://play.google.com/store/apps/details?id=com.google.android.gms">Google Play Services</a> is included with your device to provide system services.
+        </li>
+        <li>
+            <a href="https://firebase.google.com/">Firebase Notifications</a> is used to send you notifications from Telegram.
+        </li>
+    </ul>
+    <h2>User Generated Content</h2>
+    <p>
+        The information that you publish in public chats or channels are available to other users of Telegram Messenger Inc.
+        You mustn't include offensive, phishing, spam and other kinds of abuse and violations of <a href ="https://telegram.org/privacy#5-3-spam-and-abuse">Telegram’s Terms of Service</a>.
+    </p>
+    <p>
+        Telegram Messenger Inc. can permanently delete your account and all data about it in case you violate the rules for posting public or receiving content, below are the actions that you can do in case you think someone doesn't respect this terms
+    </p>
+    <ul>
+        <li>
+            <b>Filtration Method of Unwanted Material:</b>
+            All information published by you or other users is stored and processed by Telegram.org servers. Telegram Messenger Inc. is developing algorithms that automatically detect inappropriate content and block access to it.
+        </li>
+        <li>
+            <b>Offensive Content Reporting Mechanism:</b>
+            You can report abusive user behavior through the context menu when highlighting a post or on the target user's profile page.
+        </li>
+        <li>
+            <b>Blocking offensive users from the service:</b>
+            You can block any user, after which you will not receive any messages from him. The blocking is available on the target user's profile page.
+        </li>
+    </ul>
+    <h2>
+        Log Data
+    </h2>
+    <p>
+        OwlGram automatically collect data in case of any error, the Log Data include information like: App Version, Device Information, OS Version, Crash Date, Language of the app, and
+        other statistics. We don't include your ("IP") address in the Log Data.
+        Log Data won't automatically send to us
+    </p>
+    <h2>
+        Children's Privacy
+    </h2>
+    <p>
+        OwlGram isn't intended for children under the age of 13, we don't knowingly collect any data via our Services from anyone under 13.
+        In case of you're in Europe (EEA) we don't collect any data for children under the age of 16, it can be changed only if the legal guardian instructs so in writing.<br><br>
+        If you're a parent or a legal guardian, and you're aware that your children provided us certain personal information, please contact us at: <a href="mailto:support@owlgram.org">support@owlgram.org</a>.
+    </p>
+    <h2>
+        Changes to this Privacy Policy
+    </h2>
+    <p>
+        OwlGram as an unofficial Telegram client is based on the official Telegram.<br>
+        So in order to learn more about it, please check <a href="https://telegram.org/privacy">Telegram Privacy Policy</a>.
+    </p>
+    <p>
+        We reserve the right to revise and modify this policy at our sole discretion.
+        You should check this page frequently in order to stay updated on the latest version
+    </p>
+    <ul>
+        <li>
+            <b>07 November, 2022:</b> Added USG ("User Generated Content") Section, added this section in "Changes to this Privacy Policy" Section, removed "Contact Us" Section and changed position of "Questions or concerns?" Section.
+        </li>
+    </ul>
+    <h2>Questions or concerns?</h2>
+    <p>
+        Reading this privacy notice will help you understand your privacy rights and choices.<br>
+        If you do not agree with our policies and practices, please do not use our Services.<br>
+        If you still have any questions or concerns, please contact us at <a href="mailto:support@owlgram.org">support@owlgram.org</a>
+    </p>
+
+    <a class="button" href="https://owlgram.org/">
+        Home Page
+    </a>
+</div>
+</body>
+</html>
